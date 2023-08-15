33a0be45da20ab3d6d9cf1f18d22c07fbfe683dc
arturscesis <arturspodzins21@gmail.com>
2023-08-15 22:35:28 +0300

    Something

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         @yield('head')
 
         <!-- Scripts -->
-        @vite('resources/css/app.css','resources/js/app.js')
+        @vite(['resources/css/app.css','resources/js/app.js'])
 
 
     </head>
