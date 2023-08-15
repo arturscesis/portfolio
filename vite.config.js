f83b66465ae8514152d1fba93e292ffbe2eacd14
arturscesis <arturspodzins21@gmail.com>
2023-08-15 19:47:45 +0300

    finished

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: 'https://walrus-app-dfdyu.ondigitalocean.app/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        
     ],
 });
